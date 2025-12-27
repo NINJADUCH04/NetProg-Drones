dcdc0769edb8eff134970985c67326b7f176f344
Nisreenghassan <nsr0227946@ju.edu.jo>
2025-12-28 02:02:17 +0300

    My latest client code

--- demo/src/main/java/com/example/Client/client.java
+++ demo/src/main/java/com/example/Client/client.java
@@ -1,9 +1,118 @@
 package com.example.Client;
+import com.example.Model.Drone;
+import com.example.Model.Task;
 
-public class client {
-    try{
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Random;
+
+
+public class Client extends Thread{
+    private DatagramSocket datagramSocket;
+    private InetAddress inetAddress;
+    private final int severPort = 9876;
+    private Drone droneData;
+    
+    public Client(DatagramSocket datagramSocket, InetAddress inetAddress, String droneID){
+        this.datagramSocket = datagramSocket;
+        this.inetAddress = inetAddress;
+        this.droneData = new Drone(droneID);
+    }
+    
+    public void Register(){
+        try{
+            String registerMessage = "REGISTER:" + droneData.getDroneID();
+            byte[] registerBuffer = registerMessage.getBytes();
+            DatagramPacket datagramPacket = new DatagramPacket(registerBuffer, registerBuffer.length, inetAddress, severPort);
+            datagramSocket.send(datagramPacket);
+            System.out.println("Registration sent for Drone " + droneData.getDroneID());    
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    @Override    
+    public void run(){
+        try{
+            while(droneData.getState().equals("ALIVE")){
+                String heartbeatMessage = droneData.getDroneID() + ";HEARTBEAT," + droneData.getState() + ";";
+                byte [] hbBuffer = heartbeatMessage.getBytes();
+
+                DatagramPacket hpPacket = new DatagramPacket(hbBuffer, hbBuffer.length, inetAddress, severPort);
+                datagramSocket.send(hpPacket);
+
+                Thread.sleep(3000);
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void handleTaskCycle(){
+        try{
+            String requestMessage = droneData.getDroneID() + ";REQUEST_TASK;";
+            byte [] requestBuffer = requestMessage.getBytes();
+            DatagramPacket rtPacket = new DatagramPacket(requestBuffer, requestBuffer.length, inetAddress, severPort);
+            datagramSocket.send(rtPacket);
+
+            byte[] receiveBuffer = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+            datagramSocket.receive(receivePacket);
+
+            String receivedTask = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            String[] receivedTaskArray =  receivedTask.split(";");
+
+            String receivedTaskID = receivedTaskArray[0];
+            String result = droneData.getDroneID() + ";SUBMIT_RESULT;" + receivedTaskID + ";";
+
+            if (receivedTaskArray.length >= 2){
+                Random random = new Random();
+                for(int i=0; i < 4; i++){
+                    int numberOfsurvivors = random.nextInt(51);
+                    if(i == 3)
+                        result += numberOfsurvivors;
+                    else
+                        result += numberOfsurvivors + ",";
+                }
+                result += ";";
+            }
+            
+            
+            byte [] sendingTaskResultBuffer = result.getBytes();
+            DatagramPacket completedTaskPacket = new DatagramPacket(sendingTaskResultBuffer, sendingTaskResultBuffer.length, inetAddress, severPort);
+            datagramSocket.send(completedTaskPacket);
+            System.out.println("Task " + receivedTaskID + " finished and results sent.");
+
+        }catch(IOException e){
+            System.out.println("Task Error: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws SocketException, UnknownHostException{
+        DatagramSocket datagramSocket = new DatagramSocket();
+        InetAddress inetAddress = InetAddress.getByName("localhost");
         
-    }catch(Exception e){
-        System.out.println(e.getMessage());
+        int i = 100;
+        while(true){
+            i++;
+            String DroneID = "DRONE-" + i;
+            Client client = new Client(datagramSocket, inetAddress, DroneID);
+            client.register();
+            client.start();
+
+            new Thread(() -> {
+            client.handleTaskCycle();
+            }).start();
+            System.out.println("Successfully Launched: " + droneID);
+
+            try {
+            Thread.sleep(1000); 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }       
     }
 }
