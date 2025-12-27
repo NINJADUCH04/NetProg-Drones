f5e36cfe763c9ab2461478bd645a7e013bb825c4
Nisreenghassan <nsr0227946@ju.edu.jo>
2025-12-27 15:58:49 +0300

    Reorganized project structure and moved files

--- demo/src/main/java/com/example/Model/Drone.java
+++ demo/src/main/java/com/example/Model/Drone.java
@@ -1,5 +1,14 @@
 package com.example.Model;
-
-public class Drone {
     
+
+public class Drone{
+    private int droneID;
+    private String state;
+    private Task currentTask;
+    
+    public Drone(int droneID){
+        this.droneID = droneID;
+        this.state = "ALIVE";
+        this.currentTask = null;
+    }  
 }
--- demo/src/main/java/com/example/Model/Task.java
+++ demo/src/main/java/com/example/Model/Task.java
@@ -1,5 +1,29 @@
 package com.example.Model;
 
-public class Task {
+import java.util.List;
+
+class Coordinate{
+    private double x;
+    private double y;
     
+    public Coordinate(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
 }
+
+public class Task{
+    private int taskID;
+    private List<Coordinate> coordiantes;
+    private String result;
+    private boolean completed;
+    
+    public Task(int taskID, List<Coordinate> coordiantes){
+        this.taskID = taskID;
+        this.coordiantes = coordiantes;
+        this.result = result;
+        this.completed = false;
+    } 
+}
+
+
